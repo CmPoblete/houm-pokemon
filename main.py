658b97f35a2fed96269dc98ef3a31c32248c6e84
CmPoblete <cmpoblete@uc.cl>
2022-01-05 11:26:04 -0300

    change main file name

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from src.count_pokemon_by_name_filter import count_pokemon_by_name_filter
 from src.pokemon_breeding import breeding_count
 from src.type_weight import type_weight
 #-----------------------------------------------------
-#Count pokemon 
+# Count pokemon 
 
-# Este regex hace match con los nombres que tengan 'at' y exclusivamente 2 'a's en su nombre (ni mas ni menos)
-# abajo de este patron dejare otro que revise los que tengan 'at' y 2 o mas 'a's en su nombre
+# This regex searches for every pokemon that have 'at' and exactly 2 'a's in their name (no more or less)
+# below the line 10 there's a regex pattern that checks for every pokemon that have 'at' and al least 2 'a's in their name
 pattern = "^(?=[^a]*a[^a]*?a)(?![^a]*a[^a]*?a[^a]*?a)(?=.*at).*"
 # pattern = "^(?=[^a]*a[^a]*?a)(?=.*at).*"
 
@@ -14,13 +14,13 @@ count_pokemon = count_pokemon_by_name_filter(pattern)
 print("Pokemon withtwo 'a's and 'at': ", count_pokemon)
 
 #------------------------------------------------------
-#Count of posible breedings of one species
+# Count of posible breedings of one species
 
 raichu = breeding_count("raichu")
 print("Number of posible breedings with this pokemon: ", raichu)
 
 #------------------------------------------------------
-#Min and Max weight of a Pokemon type
+# Min and Max weight of a Pokemon type
 
 weights = type_weight("fighting", min_id=1, max_id=151)
 print("Max and min weight of fighting pokemons from 1'st generation: ", weights)
