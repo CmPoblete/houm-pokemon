c3bc2fa4b9b5ee477d63b524b3fefa1262f390d2
CmPoblete <cmpoblete@uc.cl>
2022-01-05 02:58:05 -0300

    add regex commentary to test file

--- test.py
+++ test.py
@@ -4,7 +4,10 @@ from src.type_weight import type_weight
 #-----------------------------------------------------
 #Count pokemon 
 
+# Este regex hace match con los nombres que tengan 'at' y exclusivamente 2 'a's en su nombre (ni mas ni menos)
+# abajo de este patron dejare otro que revise los que tengan 'at' y 2 o mas 'a's en su nombre
 pattern = "^(?=[^a]*a[^a]*?a)(?![^a]*a[^a]*?a[^a]*?a)(?=.*at).*"
+# pattern = "^(?=[^a]*a[^a]*?a)(?=.*at).*"
 
 count_pokemon = count_pokemon_by_name_filter(pattern)
 
